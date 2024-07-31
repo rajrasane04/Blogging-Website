5c5aeb59806ef81c343a51dddeeb8d83d31a362b
Raj Rasane <rasaneraj04@gmail.com>
2024-07-31 20:38:23 +0530

    changed router method to latest REACT 18 , for removing the warning in console

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot} from 'react-dom/client'
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+
+// Use createRoot instead of ReactDOM.render
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  /* </React.StrictMode> */
 );
+
